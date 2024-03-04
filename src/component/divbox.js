f854c1cf9d46465e4a355055eb8d2d2e9766dea7
zeonzeon-e <julia3277@naver.com>
2024-03-04 18:18:11 +0900

    팀 상세 페이지 메인 개발

--- src/component/divbox.js
+++ src/component/divbox.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import "./css/component.css"
-import "../index.css"
+import React from "react";
+import "./css/component.css";
+import "../index.css";
 
-function divbox(props){
-    const ItemList = props.item.map((el)=>(
-        <div className='divbox_div-content'>
-            <p className='divbox_content-title'>{el.title}</p>
-            <p className='divbox_content-date'>{el.date}</p>
+function divbox(props) {
+  const scount = 0;
+  const ItemList = props.item.map((el) => {
+    // 공지사항 divbox
+    if (!el.btn && el.date) {
+      return (
+        <div key={el.title} className="divbox_div-content_notice">
+          <p className="divbox_content-title">{el.title}</p>
+          <p className="divbox_content-date">{el.date}</p>
         </div>
-    ))
+      );
+    }
 
-    return (
-        <div className='divbox'>
-            <div className='divbox_div-title'>
-                <p className='divbox_title'>{props.title}</p>
-                <button className='divbox_button'>{props.button}</button>
+    // 경기일정 divbox
+    if (el.btn && !el.date && el.place) {
+      return (
+        <div key={el.title} className="divbox_div-content_game">
+          <p className="divbox_content-title">{el.title}</p>
+          <p>{el.place}</p>
+          <p className="divbox_content-date">{el.btn}</p>
+        </div>
+      );
+    }
+  });
+  const Teamlist = (
+    <div className="divbox_div-content_team">
+      {props.item.map((el) => {
+        return (
+          <div className="divbox_content-position">
+            <p className="content_position-p">{el.position}</p>
+            <div className={"content_" + el.position}>
+              <p className="content_count">{el.count}</p>
             </div>
-            {ItemList}
-        </div>
-    )
-    
+          </div>
+        );
+      })}
+      {scount}
+    </div>
+  );
+
+  return (
+    <div className="divbox">
+      <div className="divbox_div-title">
+        <p className="divbox_title">{props.title}</p>
+        <button className="divbox_button">{props.button}</button>
+      </div>
+      {props.title === "팀 정보" ? Teamlist : ItemList}
+    </div>
+  );
 }
 
 export default divbox;

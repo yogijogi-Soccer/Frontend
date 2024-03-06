edd01055b4f8eb70b46dc9841cd30692e9d2dc0a
seohyeon <uimi3606@gmail.com>
2024-03-06 17:25:27 +0900

    🐛 navi 이미지 깨지는 문제 해결

--- src/component/navigationbar.js
+++ src/component/navigationbar.js
@@ -6,10 +6,10 @@ function navigationbar(){
     return(
         <div>
             <div className="navigationbar">
-                <Link to="/" className="link"><div className='naviIcon'><img src='img/homeIcon.png' alt="icon"/><span>홈</span></div></Link>
-                <Link to="/find" className="link"><div className='naviIcon'><img src='img/findIcon.png' alt="icon"/><span>팀 찾기</span></div></Link>
-                <Link to="/create" className="link"><div className='naviIcon'><img src='img/createIcon.png' alt="icon"/><span>팀 생성</span></div></Link>
-                <Link to="/login" className="link"><div className='naviIcon'><img src='img/userIcon.png' alt="icon"/><span>로그인</span></div></Link>
+                <Link to="/" className="link"><div className='naviIcon'><img src='/img/homeIcon.png' alt="icon"/><span>홈</span></div></Link>
+                <Link to="/find" className="link"><div className='naviIcon'><img src='/img/findIcon.png' alt="icon"/><span>팀 찾기</span></div></Link>
+                <Link to="/create" className="link"><div className='naviIcon'><img src='/img/createIcon.png' alt="icon"/><span>팀 생성</span></div></Link>
+                <Link to="/login" className="link"><div className='naviIcon'><img src='/img/userIcon.png' alt="icon"/><span>로그인</span></div></Link>
             </div>
         </div>
     )

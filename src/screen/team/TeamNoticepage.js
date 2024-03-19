d794ade7ad46bd349a5413cae292515c543f1903
zeonzeon-e <julia3277@naver.com>
2024-03-19 18:18:03 +0900

    🔧 공지사항 페이지 글쓰기 버튼 생성

--- src/screen/team/TeamNoticepage.js
+++ src/screen/team/TeamNoticepage.js
@@ -8,6 +8,7 @@ import TeamNav from "../../component/teamNav";
 import Noticlist from "../../component/Noticelist";
 
 function TeamNoticepage() {
+  let role = true;
   const noticelist = [
     { title: "[필독] 팀 개설 안내", date: "2020.03.18 12:00", update: 5 },
     { title: "제목2", date: "2023.03.14 12:00", update: 10 },
@@ -18,6 +19,7 @@ function TeamNoticepage() {
       <Header />
       <div className="div-page">
         <TeamNav menu="공지사항" />
+        {role ? <button className="Noticediv-btn">글쓰기</button> : ""}
         <Noticlist list={noticelist} />
       </div>
       <Navigationbar />

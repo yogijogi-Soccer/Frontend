8497aa2e0a4a9770a46fdae6b9ea92136cdd987a
ZEONe <julia3471@gmail.com>
2024-06-13 16:54:55 +0900

    아이콘 변경

--- src/component/header.js
+++ src/component/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../index.css";
+import { TiArrowLeftThick } from "react-icons/ti";
 
 function Useheader({ main }) {
   return <div>{main === true ? <MainHeader /> : <MenuHeader />}</div>;
@@ -20,12 +21,11 @@ function MenuHeader() {
   const navigate = useNavigate();
   return (
     <div className="menuHeader">
-      <button
+      <TiArrowLeftThick
         style={{ marginLeft: "3vw", marginRight: "auto" }}
         onClick={() => navigate(-1)}
-      >
-        이전
-      </button>
+      />
+
       <Link to="/" className="link">
         요기조기
       </Link>
